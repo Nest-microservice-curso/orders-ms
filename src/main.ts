7cfaa1b31076aa3ab3e8fa8994671cd74f855868
LauchoIT <contacto@gmail.com>
2024-03-19 15:22:41 -0400

    Ajustando la respuesta de el error cuando el sistema tiene el sevicio caido

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { envs } from './config';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
-const logger = new Logger('Orders-Main');
+const logger = new Logger('orders-main.ts');
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
